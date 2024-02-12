7d0ed3f3ac5ffda10f85a23b637809c609401fb2
Rohit Aggarwal <rohitsag94@gmail.com>
2024-02-12 20:27:30 +0530

    ...

--- lib/utils.ts
+++ lib/utils.ts
@@ -138,6 +138,8 @@ export const jobSearch = async ({ query, filter, page }: Props) => {
     const userLocation = await locationResponse.json();
     const countriesData = await countriesResponse.json();
     let location = filter || userLocation.country;
+    console.log(location);
+
     const response = await fetch(
       `https://jsearch.p.rapidapi.com/search?query=${query}%20in%20${location}&page=${page}&num_pages=1`,
       {
